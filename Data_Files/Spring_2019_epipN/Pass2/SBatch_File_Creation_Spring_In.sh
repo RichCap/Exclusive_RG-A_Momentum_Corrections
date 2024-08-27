63a9bc3404e44c329f05cc5a73959065a4d3c4ec
richcap <richard.capobianco@uconn.edu>
2024-08-27 15:01:43 -0400

    New 'sbatch' files to make it easier to run (rather than use the existing scripts in this directory)

---
+++ Data_Files/Spring_2019_epipN/Pass2/SBatch_File_Creation_Spring_In.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --job-name=SP_P2_F_In_8_27_2024_Run1_Sp19_P2_Refine_V1_Mom_Cors
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=richard.capobianco@uconn.edu 
+#SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%A_%a-%j-%N.err
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH --mem-per-cpu=6000
+#SBATCH --time=24:00:00
+#SBATCH --array=0-119
+
+
+FILES=(/w/hallb-scshelf2102/clas12/richcap/Momentum_Corrections/Spring2019/Pass2/Inbending_recon/Single_Pion_Channel_epipN/ePip.pass2.inb.qa.rec_clas_00*)
+# (Inbending - Pass 2) array=0-119
+
+
+srun python3 /w/hallb-scshelf2102/clas12/richcap/Exclusive_RG-A_Momentum_Corrections/Data_Files/Momentum_Correction_File_Creation_wPass2.py In P2SPF ${FILES[$SLURM_ARRAY_TASK_ID]}
